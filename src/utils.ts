b312392201b3f605fb6fbaf6745396b44f14fc14
ddiu8081 <ddiu8081@gmail.com>
2022-05-02 23:53:46 +0800

    fix: expression translation

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,16 @@
+const replaceReg = (rawExpression: string) => {
+  const reg = /(\d+)([a-zA-Z]+)/g
+  return rawExpression.replace(reg, (_, p1, p2) => `${p1} * ${p2}`)
+}
+
 export function getMathFn(inputParam: string, expression: string): MathFn {
   const MathContext = `const {${Object.getOwnPropertyNames(Math).join(',')}}=Math`
-  const formatExp = `()=>{
+
+  const formatExp = `() => {
     ${MathContext};
     return (${ inputParam }) => {
-      return ${expression}
+      return ${replaceReg(expression)};
     }
   }`
-  // todo: fix this
-  // return ${expression.replace(/(\d+)(\w+)/g, (_, n, x) => `${n} * ${x}`)}
   return eval(formatExp)()
 }
